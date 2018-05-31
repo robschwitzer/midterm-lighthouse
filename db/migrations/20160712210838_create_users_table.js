a6588d8e0e55a58c7fe7ceb054c7a2ca7df89cf3
Matt Greff <greff.matt@icloud.com>
2018-05-31 00:12:13 +0000

    resetting and migrating db, and seeding db

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,57 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments('id');
-    table.string('name');
-  });
+  return Promise.all([
+    knex.schema.createTable('users', function (table) {
+      table.increments('id');
+      table.string('name');
+      table.string('email');
+      table.string('password');
+    }),
+    knex.schema.createTable('comments', function (table) {
+      table.increments('id');
+      table.text('comment');
+      table.integer('commenter_id');
+      table.integer('url_id');
+    }),
+    knex.schema.createTable('ranks', function (table) {
+      table.increments('id');
+      table.string('rank');
+      table.integer('ranker_id');
+      table.integer('url_id');
+    }),
+    knex.schema.createTable('likes', function (table) {
+      table.increments('id');
+      table.string('like');
+      table.integer('liker_id');
+      table.integer('url_id');
+    }),
+    knex.schema.createTable('urls', function (table) {
+      table.increments('id');
+      table.string('title');
+      table.string('url');
+      table.string('description');
+      table.date('created_at');
+      table.integer('creator_id');
+    }),
+    knex.schema.createTable('tagged_topics', function (table) {
+      table.increments('id');
+      table.integer('topic_id');
+      table.integer('url_id');
+    }),
+    knex.schema.createTable('topics', function (table) {
+      table.increments('id');
+      table.string('topic');
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+  return Promise.all([
+    knex.schema.dropTable('users'),
+  knex.schema.dropTable('comments'),
+  knex.schema.dropTable('ranks'),
+  knex.schema.dropTable('likes'),
+  knex.schema.dropTable('urls'),
+  knex.schema.dropTable('tagged_topics'),
+  knex.schema.dropTable('topics')
+  ])
 };
--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,17 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, name: 'Alice', password: '1234', email: 'Alice@example.com'}),
+        knex('users').insert({id: 2, name: 'Bob', password: '1234', email: 'Bob@example.com'}),
+        knex('users').insert({id: 3, name: 'Jim', password: '1234', email: 'Jim@example.com'}),
+        knex('urls').insert({id: 1, title: 'foo', description: '1234', created_at: '2012-04-03'}),
+        knex('urls').insert({id: 2, title: 'bar', description: '1234', created_at: '2012-04-03'}),
+        knex('urls').insert({id: 3, title: 'baz', description: '1234', created_at: '2012-04-03'}),
+        knex('topics').insert({id: 1, topic: 'ajax'}),
+        knex('topics').insert({id: 2, topic: 'node.js'}),
+        knex('likes').insert({id: 1, like: false, liker_id: 1, url_id: 1}),
+        knex('ranks').insert({id: 1, rank: true, ranker_id: 2, url_id: 2}),
+        knex('comments').insert({id: 1, comment: 'foo bar buzz', commenter_id: 1, url_id: 1})
       ]);
     });
 };
