1245dc6b9f32d0282fce6ebeb9dafe7f7dcb4f04
Matt Greff <greff.matt@icloud.com>
2018-05-31 06:13:59 +0000

    adding routes and server quries for rendering resources

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,43 @@
 $(() => {
+
+getDocs(getComments);
+
+});
+
+
+const getDocs = (cb) => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
+    url: "/api/urls"
+  }).done((docs) => {
+    console.log(docs)
+    docs.forEach((doc) => {
+      
+      const $title = $("<h1>").text(doc.title),
+      $description = $("<p>").text(doc.$description),
+      $url = $("<a>").text(doc.url).attr('href', doc.url),
+      $doc = $('<div>').append($title, $description, $url).addClass('resource');
+      
+      cb(doc.id, $doc);
+
+      $doc.appendTo('body');
+    });
   });;
-});
+}
+
+const getComments = (doc_id, $doc_div) => {
+  $.ajax({
+    method: "GET",
+    url: "/api/comments"
+  }).done((comments) => {
+    console.log(comments)
+    comments.forEach((packet) => {
+      if(doc_id === packet.url_id) {
+        $comment = $("<div>")
+        .text(packet.comment)
+        .appendTo($doc_div)
+        .addClass('comment');
+      }
+    });
+  });
+}
--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+const urlsRoutes = require("./routes/urls");
+const commentsRoutes = require("./routes/comments");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -37,6 +39,8 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
+app.use("/api/urls", urlsRoutes(knex));
+app.use("/api/comments", commentsRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
