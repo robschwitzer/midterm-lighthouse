df2894b91fe91a81d725c074bad35753926005f8
Matt Greff <greff.matt@icloud.com>
2018-06-04 16:23:47 +0000

    heroku

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,14 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: process.env.DB_SSL
     },
+
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
@@ -22,8 +23,8 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: 'postgres://bpfebddiyxdiko:bee6cb8d76b5d41b136aaa5c4cafffedd92d62438c8a9adb0ec8af80f220f8cc@ec2-54-83-59-120.compute-1.amazonaws.com:5432/d9gm8jpp0u0ump' + '?ssl=true',
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
