35b772daa996bb4c01d68dfdd0935ba689060734
Matt Greff <greff.matt@icloud.com>
2018-06-04 15:22:32 +0000

    adding seeded users for demo

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,8 +2,8 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id:6, name:'Matty', email:'Mattischad@example.com', password:'1234'}),
-        knex('users').insert({id:6, name:'Matty', email:'Mattischad@example.com', password:'1234'})
+        knex('users').insert({id:1, name:'Matty', email:'matt@example.com', password:'1234'}),
+        knex('users').insert({id:2, name:'Rob', email:'rob@example.com', password:'1234'})
       ]);
     });
 };
