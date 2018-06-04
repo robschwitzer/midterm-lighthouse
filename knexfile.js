cb92807c33168ce215fc084ff37f0071a4528260
Matt Greff <greff.matt@icloud.com>
2018-06-04 17:21:29 +0000

    heroku

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 
 module.exports = {
 
@@ -24,7 +24,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: 'postgres://bpfebddiyxdiko:bee6cb8d76b5d41b136aaa5c4cafffedd92d62438c8a9adb0ec8af80f220f8cc@ec2-54-83-59-120.compute-1.amazonaws.com:5432/d9gm8jpp0u0ump' + '?ssl=true',
     pool: {
       min: 2,
       max: 10
