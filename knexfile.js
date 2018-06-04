1ba87e164413a1e8bddc6aafab4f1b0b38c62641
Matt Greff <greff.matt@icloud.com>
2018-06-04 15:47:53 +0000

    more more more heroku

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: 'postgres://bpfebddiyxdiko:bee6cb8d76b5d41b136aaa5c4cafffedd92d62438c8a9adb0ec8af80f220f8cc@ec2-54-83-59-120.compute-1.amazonaws.com:5432/d9gm8jpp0u0ump' + '?ssl=true',
     pool: {
       min: 2,
